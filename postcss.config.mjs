546b5e6984acca8a12e23a62ddfb0ab33c3ec7a8
jengerred <56004257+jengerred@users.noreply.github.com>
2025-02-15 19:39:20 -0500

    button color

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -5,18 +5,4 @@ const config = {
   },
 };
 
-export default {
-  headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'X-XSS-Protection', value: '1; mode=block' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' }
-        ]
-      }
-    ];
-  }
-};
-
+export default config;
